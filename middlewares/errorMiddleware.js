48f80aafb441a40920d0d265ebdfbdbde544ee43
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2024-04-27 20:38:33 +0530

    Delete middlewares directory

--- middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errMessage = err.message || "Something went wrong!";
-
-  res.status(errorStatus).json({
-    message: errMessage,
-    stack: process.env.NODE_ENV === "development" ? err.stack : null,
-  });
-};
