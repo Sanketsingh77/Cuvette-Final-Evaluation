a5145132cd5c37aa871cbb836cbf51e643bf3887
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2024-04-27 20:39:17 +0530

    Delete src directory

--- src/api/getPostsByCat.js
+++
@@ -1,9 +0,0 @@
-import newRequest from "../utils/newRequest";
-
-const getPostsByCat = async (cat) => {
-  const data = await newRequest.get(`/post/cat/${cat}`);
-  // console.log(data);
-  return data;
-};
-
-export default getPostsByCat;
--- src/utils/newRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const newRequest = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-  withCredentials: true,
-});
-
-export default newRequest;
