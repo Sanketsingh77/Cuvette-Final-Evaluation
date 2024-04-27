1019b655f06801095190ecef380b764c846c8ed3
Sanketsingh77 <35081012+Sanketsingh77@users.noreply.github.com>
2024-04-27 13:43:42 +0530

    create react app

---
+++ src/utils/cardsData.js
@@ -0,0 +1,32 @@
+export const categoryData = [
+  {
+    id: "9",
+    categoryName: "All",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1_5qL45U5p--LP_0J5hZTR0D4PWdftpeJ1z2HyLRE-g&s",
+  },
+  {
+    id: "1",
+    categoryName: "Food",
+    img: "https://media.istockphoto.com/id/1457433817/photo/group-of-healthy-food-for-flexitarian-diet.jpg?b=1&s=612x612&w=0&k=20&c=V8oaDpP3mx6rUpRfrt2L9mZCD0_ySlnI7cd4nkgGAb8=",
+  },
+  {
+    id: "2",
+    categoryName: "Health",
+    img: "https://images.everydayhealth.com/images/everything-you-need-know-about-fitness-1440x810.jpg",
+  },
+  {
+    id: "3",
+    categoryName: "Travel",
+    img: "https://t4.ftcdn.net/jpg/00/65/48/25/360_F_65482539_C0ZozE5gUjCafz7Xq98WB4dW6LAhqKfs.jpg",
+  },
+  {
+    id: "4",
+    categoryName: "Movies",
+    img: "https://img.freepik.com/premium-vector/movie-theater-signboard-blue_34230-295.jpg",
+  },
+  {
+    id: "5",
+    categoryName: "Education",
+    img: "https://cdn.elearningindustry.com/wp-content/uploads/2022/02/shutterstock_1112381495.jpg",
+  },
+];
